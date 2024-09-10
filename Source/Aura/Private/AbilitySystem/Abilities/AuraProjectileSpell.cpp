58d45d445c313df2cf85b7a52b812273d30bed2b
Mezz <mezzanine510@gmail.com>
2024-09-09 18:58:41 -0700

    implemented basic firebolt casting loop

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -3,7 +3,8 @@
 
 #include "AbilitySystem/Abilities/AuraProjectileSpell.h"
 
-#include "Kismet/KismetSystemLibrary.h"
+#include "Actor/AuraProjectile.h"
+#include "Interaction/CombatInterface.h"
 
 
 void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
@@ -13,6 +14,29 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
 
-	UKismetSystemLibrary::PrintString(this, FString("ActivateAbility (C++"), true, true, FLinearColor::Yellow, 5.0f);
-	
+	UE_LOG(LogTemp, Warning, TEXT("UAuraProjectileSpell::ActivateAbility"));
+	const bool bIsServer = HasAuthority(&ActivationInfo);
+	if (!bIsServer) return;
+	UE_LOG(LogTemp, Warning, TEXT("IsServer"));
+
+	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
+	if (CombatInterface)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("CombatInterface is valid"));
+		const FVector SocketLocation = CombatInterface->GetCombatSocketLocation();
+		FTransform SpawnTransform;
+		SpawnTransform.SetLocation(SocketLocation);
+		// TODO: Set the rotation of the SpawnTransform to face the target
+		
+		AAuraProjectile* Projectile = GetWorld()->SpawnActorDeferred<AAuraProjectile>(
+			ProjectileClass,
+			SpawnTransform,
+			GetAvatarActorFromActorInfo(),
+			Cast<APawn>(GetAvatarActorFromActorInfo()),
+			ESpawnActorCollisionHandlingMethod::AlwaysSpawn);
+
+		// TODO: Give the projectile a GameplayEffectSpec for causing Damage
+
+		Projectile->FinishSpawning(SpawnTransform);
+	}
 }
--- Source/Aura/Private/Actor/AuraProjectile.cpp
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -10,6 +10,7 @@
 AAuraProjectile::AAuraProjectile()
 {
 	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
 
 	Sphere = CreateDefaultSubobject<USphereComponent>("Sphere");
 	SetRootComponent(Sphere);
