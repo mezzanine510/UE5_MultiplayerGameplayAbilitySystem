4a1c65085703ed6981a69ae6d52d09dfecbb0d36
Mezz <mezzanine510@gmail.com>
2024-09-11 13:07:21 -0700

    sending gameplay events using animnotify to activate FireBolt ability

--- Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -13,16 +13,16 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
                                            const FGameplayEventData* TriggerEventData)
 {
 	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+}
 
-	UE_LOG(LogTemp, Warning, TEXT("UAuraProjectileSpell::ActivateAbility"));
-	const bool bIsServer = HasAuthority(&ActivationInfo);
+void UAuraProjectileSpell::SpawnProjectile()
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer) return;
-	UE_LOG(LogTemp, Warning, TEXT("IsServer"));
 
 	ICombatInterface* CombatInterface = Cast<ICombatInterface>(GetAvatarActorFromActorInfo());
 	if (CombatInterface)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("CombatInterface is valid"));
 		const FVector SocketLocation = CombatInterface->GetCombatSocketLocation();
 		FTransform SpawnTransform;
 		SpawnTransform.SetLocation(SocketLocation);
