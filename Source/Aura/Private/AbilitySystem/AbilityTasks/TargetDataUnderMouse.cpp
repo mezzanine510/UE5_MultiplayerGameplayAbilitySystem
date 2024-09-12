f4a0342146e8f6b837d289461645c2837fa90d69
Mezz <mezzanine510@gmail.com>
2024-09-11 20:55:02 -0700

    sending abilitytask data for target under cursor from client to server

--- Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
 
+#include "AbilitySystemComponent.h"
+
 
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
@@ -12,8 +14,42 @@ UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGamepl
 
 void UTargetDataUnderMouse::Activate()
 {
+	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+	if (bIsLocallyControlled)
+	{
+		SendMouseCursorData();
+	}
+	else
+	{
+		// TODO: We are on the server, so listen for target data from the client
+		
+	}
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+	// Allows client/server to scope prediction, so that client can handle its logic locally, then server will validate and run when appropriate
+	FScopedPredictionWindow ScopedPredictionWindow(AbilitySystemComponent.Get());
+	
 	FHitResult CursorHit;
 	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
 	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
-	ValidData.Broadcast(CursorHit.ImpactPoint);
+	
+	FGameplayAbilityTargetDataHandle DataHandle;
+	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();
+	Data->HitResult = CursorHit;
+	DataHandle.Add(Data);
+	
+	AbilitySystemComponent->ServerSetReplicatedTargetData(
+		GetAbilitySpecHandle(),
+		GetActivationPredictionKey(), 
+		DataHandle,
+		FGameplayTag(),
+		AbilitySystemComponent->ScopedPredictionKey
+	);
+
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
 }
--- Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
+++ Source/Aura/Public/AbilitySystem/AbilityTasks/TargetDataUnderMouse.h
@@ -7,7 +7,7 @@
 #include "TargetDataUnderMouse.generated.h"
 
 
-DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FVector&, Data);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FMouseTargetDataSignature, const FGameplayAbilityTargetDataHandle&, DataHandle);
 
 
 /**
@@ -28,4 +28,5 @@ public:
 
 private:
 	virtual void Activate() override;
+	void SendMouseCursorData();
 };
