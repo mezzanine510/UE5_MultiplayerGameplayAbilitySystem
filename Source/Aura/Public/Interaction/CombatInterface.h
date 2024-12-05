9c8b6f0743d25863456f502132568e922fd51f18
Mezz <mezzanine510@gmail.com>
2024-12-05 14:55:13 -0800

    character now turns in direction of fireball casting target

--- Source/Aura/Public/Interaction/CombatInterface.h
+++ Source/Aura/Public/Interaction/CombatInterface.h
@@ -24,4 +24,7 @@ class AURA_API ICombatInterface
 public:
 	virtual int32 GetCharacterLevel();
 	virtual FVector GetCombatSocketLocation();
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+	void UpdateFacingTarget(const FVector& TargetLocation);
 };
